39153bc132cb4b934889ddcb9d2d4f8d5904c360
LaoDu <2453755913.@qq.com>
2020-10-12 10:37:35 +0800

    修改多余代码

--- pages/air/index.vue
+++ pages/air/index.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>机票首页</div>
+  <div>
+    <transition>
+      <div>机票首页</div>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="less" scoped>
+div {
+  color: red;
+}
 </style>
